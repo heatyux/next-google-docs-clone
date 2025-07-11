d0b77fbc42759bc0aeaee58fd0d95ce104de2829
heatyux <421743974@qq.com>
2025-07-11 11:30:43 +0800

    feat: setting tiptap extensions and styles

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,15 @@
 'use client'
 
+import Image from '@tiptap/extension-image'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import ImageResize from 'tiptap-extension-resize-image'
 
 export const Editor = () => {
   const editor = useEditor({
@@ -12,8 +20,18 @@ export const Editor = () => {
           'focus:outline-none bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] py-10 pr-14 cursor-text print:border-0',
       },
     },
-    extensions: [StarterKit],
-    content: '<p>Hello World! 🌍</p>',
+    extensions: [
+      Image,
+      ImageResize,
+      StarterKit,
+      Table.configure({ resizable: true }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      TaskList,
+      TaskItem.configure({ nested: true }),
+    ],
+    content: '<h1>Hello World!</h1>',
     immediatelyRender: false,
   })
 
