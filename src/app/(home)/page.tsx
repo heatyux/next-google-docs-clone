d13b32f7d42a98ae1ee7ba978a069ab3beaee981
heatyux <421743974@qq.com>
2025-07-22 10:26:01 +0800

    feat: convex schema created

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,14 @@
+'use client'
+
+import { useQuery } from 'convex/react'
+
+import { api } from '../../../convex/_generated/api'
 import { Navbar } from './navbar'
 import { TemplateGallery } from './template-gallery'
 
 const HomePage = () => {
+  const documents = useQuery(api.documents.get)
+
   return (
     <div className="flex min-h-screen flex-col">
       <div className="fixed inset-x-0 top-0 z-10 h-16 bg-white p-4">
@@ -9,6 +16,9 @@ const HomePage = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   )
